e04bd7456f4f03f22a7ca2f76cab7738866be803
karanja22 <frasiahnyambura22@gmail.com>
2025-06-26 18:56:47 +0300

    refactor: remove electrol-commision component and its related files
    - Deleted electrol-commision component files: HTML, CSS, TypeScript, and spec files.
    - Added app.component files including HTML, CSS, TypeScript, and spec files.
    - Updated app.component to include a title and basic structure.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,8 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([]))
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
